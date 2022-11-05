0031b13dd81d8660ad2912cc4aa7916bcd9b3f1e
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-11-05 22:02:12 +0100

    Update sketchBolitasVoladoras.js

--- sketchBolitasVoladoras.js
+++ sketchBolitasVoladoras.js
@@ -1,7 +1,7 @@
 let bolitasVoladoras = new Array();
 let colores = ['red', 'green', 'blue', 'yellow'];
 let total = 10;
-let velocidad = 2.5;
+let velocidad = 2;
 
 
 //corre solo una vez cuando inicia el programa
@@ -41,10 +41,13 @@ function totalBolitas(){
     clear();
     bolitasVoladoras = [];
     this.total = document.getElementById("rangoID").value;
-    while(this.total > 0){
+    total_ = this.total;
+    while(total_ > 0){
         bolitasVoladoras.push(new BolitaVoladora(random(0,800),random(0,800), random(5,100),colores[Math.floor(Math.random()*colores.length)]));
-        this.total -=1;
+        total_ -=1;
     }
+    
+    document.getElementById("numBolasSpanID").innerHTML = this.total;
 }
 function totalVelocidad(){
     alert( document.getElementById("rangovelocidadID").value);
@@ -55,6 +58,8 @@ function totalVelocidad(){
         bolitaVoladora.setVelocidad(this.velocidad);
         
     });
+
+    document.getElementById("velocidadSpanID").innerHTML =  this.velocidad;
 }
 
 class BolitaVoladora{
@@ -73,12 +78,14 @@ class BolitaVoladora{
    
     mostrar(){
     if(mouseIsPressed){
+        ellipse(this.x,this.y,this.diametro,this.diametro);
         fill(colores[Math.floor(Math.random()*colores.length)]);
     }else{
+        rect(this.x,this.y,this.diametro,this.diametro);
         fill(this.color);
     }
-       // 
-        ellipse(this.x,this.y,this.diametro,this.diametro);
+  
+        
     }
 
     setVelocidad(velocidad){
